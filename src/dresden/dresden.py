a9a46e742b7eefd630e6801fbacd2b4f6a4eb1e4
Klaas Schoute <klaas_schoute@hotmail.com>
2023-12-19 01:29:31 +0100

    Fix import and update async timeout (#338)

--- src/dresden/dresden.py
+++ src/dresden/dresden.py
@@ -5,9 +5,8 @@ import asyncio
 import socket
 from dataclasses import dataclass
 from importlib import metadata
-from typing import TYPE_CHECKING, Any, cast
+from typing import Any, Self, cast
 
-import async_timeout
 from aiohttp import ClientError, ClientSession
 from aiohttp.hdrs import METH_GET
 from yarl import URL
@@ -15,13 +14,10 @@ from yarl import URL
 from .exceptions import ODPDresdenConnectionError, ODPDresdenError
 from .models import DisabledParking
 
-if TYPE_CHECKING:
-    from typing_extensions import Self
-
 
 @dataclass
 class ODPDresden:
-    """Main class for handling data fetchting from Open Data Platform of Dresden."""
+    """Main class for handling data fetching from Open Data Platform of Dresden."""
 
     request_timeout: float = 10.0
     session: ClientSession | None = None
@@ -71,7 +67,7 @@ class ODPDresden:
             self._close_session = True
 
         try:
-            async with async_timeout.timeout(self.request_timeout):
+            async with asyncio.timeout(self.request_timeout):
                 response = await self.session.request(
                     method,
                     url,
