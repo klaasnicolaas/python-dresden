253c870ba778071f6f944979935f0526d446ba39
Klaas Schoute <klaas_schoute@hotmail.com>
2024-02-13 17:15:37 +0100

    Update target version in pyproject.toml and handle TimeoutError in ODPDresden class

--- src/dresden/dresden.py
+++ src/dresden/dresden.py
@@ -77,7 +77,7 @@ class ODPDresden:
                     ssl=True,
                 )
                 response.raise_for_status()
-        except asyncio.TimeoutError as exception:
+        except TimeoutError as exception:
             msg = "Timeout occurred while connecting to the Open Data Platform API."
             raise ODPDresdenConnectionError(
                 msg,
