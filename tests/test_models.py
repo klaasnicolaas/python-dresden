00bd3e96591208d693a16aa0fd9a64e4293b59ff
Klaas Schoute <klaas_schoute@hotmail.com>
2024-11-09 23:11:32 +0100

    Refactor test_all_parking_spaces to use snapshot assertions for validation

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,15 +2,21 @@
 
 from __future__ import annotations
 
-from aresponses import ResponsesMockServer
+from typing import TYPE_CHECKING
 
-from dresden import DisabledParking, ODPDresden
+from aresponses import ResponsesMockServer
+from syrupy.assertion import SnapshotAssertion
 
 from . import load_fixtures
 
+if TYPE_CHECKING:
+    from dresden import DisabledParking, ODPDresden
+
 
 async def test_all_parking_spaces(
-    aresponses: ResponsesMockServer, odp_dresden_client: ODPDresden
+    aresponses: ResponsesMockServer,
+    snapshot: SnapshotAssertion,
+    odp_dresden_client: ODPDresden,
 ) -> None:
     """Test all parking spaces function."""
     aresponses.add(
@@ -24,13 +30,4 @@ async def test_all_parking_spaces(
         ),
     )
     spaces: list[DisabledParking] = await odp_dresden_client.disabled_parkings()
-    assert spaces is not None
-    for item in spaces:
-        assert isinstance(item, DisabledParking)
-        assert item.entry_id is not None
-        assert isinstance(item.entry_id, int)
-        assert item.number is None or item.number >= 1
-        assert item.longitude is not None
-        assert isinstance(item.longitude, float)
-        assert item.latitude is not None
-        assert isinstance(item.latitude, float)
+    assert spaces == snapshot
